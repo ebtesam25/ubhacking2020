03712d7bc36ceb93d7a6cfef795697889998957a
ebtesam25 <ebtesam@iut-dhaka.edu>
2020-10-25 18:07:09 +0300

    dashboard

---
+++ dashboard/variables/charts.js
@@ -0,0 +1,301 @@
+// ##############################
+// // // javascript library for creating charts
+// #############################
+var Chartist = require("chartist");
+
+// ##############################
+// // // variables used to create animation on charts
+// #############################
+var delays = 80,
+  durations = 500;
+var delays2 = 80,
+  durations2 = 500;
+
+// ##############################
+// // // Daily Sales
+// #############################
+
+const dailySalesChart = {
+  data: {
+    labels: [
+      
+      "1st",
+      "2nd",
+      "3rd",
+      "4th",
+      "5th",
+     
+      
+    ],
+    series: [[
+      10,
+      20,
+      20,
+      20,
+      20
+  ]]
+  },
+  options: {
+    axisX: {
+      showGrid: false
+    },
+    low: 0,
+    high: 100,
+    chartPadding: {
+      top: 0,
+      right: 5,
+      bottom: 0,
+      left: 0
+    }
+  },
+  responsiveOptions: [
+    [
+      "screen and (max-width: 640px)",
+      {
+        seriesBarDistance: 5,
+        axisX: {
+          labelInterpolationFnc: function(value) {
+            return value[0];
+          }
+        }
+      }
+    ]
+  ],
+  animation: {
+    draw: function(data) {
+      if (data.type === "bar") {
+        data.element.animate({
+          opacity: {
+            begin: (data.index + 1) * delays2,
+            dur: durations2,
+            from: 0,
+            to: 1,
+            easing: "ease"
+          }
+        });
+      }
+    }
+  }
+};
+
+// ##############################
+// // // Email Subscriptions
+// #############################
+
+const emailsSubscriptionChart = {
+  data: {
+    labels: [
+      
+      "1st",
+      "2nd",
+      "3rd",
+      "4th",
+      "5th",
+     
+      
+    ],
+    series: [[75,
+      72,
+      80,
+      68,
+      77]]
+  },
+  options: {
+    axisX: {
+      showGrid: false
+    },
+    low: 0,
+    high: 100,
+    chartPadding: {
+      top: 0,
+      right: 5,
+      bottom: 0,
+      left: 0
+    }
+  },
+  responsiveOptions: [
+    [
+      "screen and (max-width: 640px)",
+      {
+        seriesBarDistance: 5,
+        axisX: {
+          labelInterpolationFnc: function(value) {
+            return value[0];
+          }
+        }
+      }
+    ]
+  ],
+  animation: {
+    draw: function(data) {
+      if (data.type === "bar") {
+        data.element.animate({
+          opacity: {
+            begin: (data.index + 1) * delays2,
+            dur: durations2,
+            from: 0,
+            to: 1,
+            easing: "ease"
+          }
+        });
+      }
+    }
+  }
+};
+
+// ##############################
+// // // Completed Tasks
+// #############################
+
+const completedTasksChart = {
+  data: {
+    labels: [
+      
+      "1",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      
+    ],
+    series: [[ 72,
+      71,
+      71,
+      74,
+      71,
+      71,
+      71,
+      70,
+      71,
+      71]]
+  },
+  options: {
+    axisX: {
+      showGrid: false
+    },
+    low: 0,
+    high: 100,
+    chartPadding: {
+      top: 0,
+      right: 5,
+      bottom: 0,
+      left: 0
+    }
+  },
+  responsiveOptions: [
+    [
+      "screen and (max-width: 640px)",
+      {
+        seriesBarDistance: 5,
+        axisX: {
+          labelInterpolationFnc: function(value) {
+            return value[0];
+          }
+        }
+      }
+    ]
+  ],
+  animation: {
+    draw: function(data) {
+      if (data.type === "bar") {
+        data.element.animate({
+          opacity: {
+            begin: (data.index + 1) * delays2,
+            dur: durations2,
+            from: 0,
+            to: 1,
+            easing: "ease"
+          }
+        });
+      }
+    }
+  }
+};
+
+
+// ##############################
+// // // Completed Tasks
+// #############################
+
+const TemperatureChart = {
+  data: {
+    labels: [
+      
+      "1",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      
+    ],
+    series: [[   71,
+      70,
+      70,
+      74,
+      72,
+      70,
+      70,
+      68,
+      70,
+      71]]
+  },
+  options: {
+    axisX: {
+      showGrid: false
+    },
+    low: 0,
+    high: 100,
+    chartPadding: {
+      top: 0,
+      right: 5,
+      bottom: 0,
+      left: 0
+    }
+  },
+  responsiveOptions: [
+    [
+      "screen and (max-width: 640px)",
+      {
+        seriesBarDistance: 5,
+        axisX: {
+          labelInterpolationFnc: function(value) {
+            return value[0];
+          }
+        }
+      }
+    ]
+  ],
+  animation: {
+    draw: function(data) {
+      if (data.type === "bar") {
+        data.element.animate({
+          opacity: {
+            begin: (data.index + 1) * delays2,
+            dur: durations2,
+            from: 0,
+            to: 1,
+            easing: "ease"
+          }
+        });
+      }
+    }
+  }
+};
+
+
+module.exports = {
+  dailySalesChart,
+  emailsSubscriptionChart,
+  completedTasksChart,
+  TemperatureChart
+};
